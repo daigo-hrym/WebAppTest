3da19d34740170acd10fa78389540acc4175f50e
daigo-hrym <129356065+daigo-hrym@users.noreply.github.com>
2024-06-11 16:00:36 +0900

    削除前最新ファイル

--- startup.sh
+++ startup.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+# 環境変数の確認
+echo "PORT before setting: $PORT" >> /home/LogFiles/startup.log
+
 # 必要な環境変数をエクスポート
 export PATH=$PATH:/home/.local/bin
-export PORT=${PORT:-5000}
+
+# 環境変数を 61234 に強制的に設定
+export PORT=61234
+echo "PORT after setting to 61234: $PORT" >> /home/LogFiles/startup.log
 
 # gunicornの起動コマンド
 exec gunicorn --bind=0.0.0.0:$PORT --timeout 600 app:app
