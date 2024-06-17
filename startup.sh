313959ec06cf2ab7c939dbe8c4a22aa456d7c738
daigo-hrym <129356065+daigo-hrym@users.noreply.github.com>
2024-06-17 17:19:13 +0900

    ログレベルを詳細化

--- startup.sh
+++ startup.sh
@@ -7,4 +7,4 @@ echo "PORT before setting: $PORT" >> /home/LogFiles/startup.log
 export PATH=$PATH:/home/.local/bin
 
 # gunicornの起動コマンド
-exec gunicorn --bind=0.0.0.0:$PORT --timeout 600 app:app
+exec gunicorn --bind=0.0.0.0:$PORT --timeout 600 --log-level debug app:app
