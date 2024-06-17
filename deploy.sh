7911d5b2b21a3417df0b6cc4db481db17ef0fb80
daigo-hrym <129356065+daigo-hrym@users.noreply.github.com>
2024-06-17 17:26:16 +0900

    インストールログ出力

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install dependencies
-pip install -r requirements.txt
+pip install -r requirements.txt | tee /home/LogFiles/pip_install.log
 
 # Start the Gunicorn server
 exec sh startup.sh
