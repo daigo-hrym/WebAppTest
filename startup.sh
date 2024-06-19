2e505f1f3bbafceb86cfc141e333cb4f910233ca
daigo-hrym <129356065+daigo-hrym@users.noreply.github.com>
2024-06-19 17:42:05 +0900

    PORT＝61234を明示。リポジトリディレクトリへの移動を追加

--- startup.sh
+++ startup.sh
@@ -1,10 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 # 環境変数の確認
 echo "PORT before setting: $PORT" >> /home/LogFiles/startup.log
 
 # 必要な環境変数をエクスポート
 export PATH=$PATH:/home/.local/bin
+
+# PORT 環境変数を設定
+export PORT=61234
+
+# 環境変数の確認
+echo "PORT after setting: $PORT" >> /home/LogFiles/startup.log
+
+# リポジトリのディレクトリに移動
+cd /home/site/wwwroot
 
 # gunicornの起動コマンド
 exec gunicorn --bind=0.0.0.0:$PORT --timeout 600 --log-level debug app:app
